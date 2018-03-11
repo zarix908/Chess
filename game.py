44741e897bb405a93f3d995c05b7aa396b1d3a40
zarix908 <daniyarrus@gmail.com>
2018-03-11 16:26:07 +0500

    add verify on check in possible moves

--- game.py
+++ game.py
@@ -7,7 +7,8 @@ from moves_getter import MovesGetter
 class Game:
     def __init__(self):
         self.__current_state = GameState()
-        self.__moves = []
+        self.__past_moves = []
+        self.__all_possible_moves = {}
 
         self.__moves_getter = MovesGetter()
         self.__predict_filter = PredictiveFilter()
@@ -25,18 +26,37 @@ class Game:
             new_state = self.__post_filter.filter(new_state, move)
 
             if new_state is not None:
-                self.__current_state = new_state
-                self.__moves.append(move)
+                self.change_game_state(new_state, move)
+
+    def change_game_state(self, new_state, last_move):
+        self.__current_state = new_state
+        self.__past_moves.append(last_move)
+        del self.__all_possible_moves
+        self.__all_possible_moves = {}
 
     def get_possible_moves(self, cell):
+        if cell not in self.__all_possible_moves:
+            self.generate_possible_moves(cell)
+
+        return self.__all_possible_moves[cell]
+
+    def generate_possible_moves(self, cell):
         piece = self.__current_state.get(cell)
         moves = self.__moves_getter.get_moves(cell, piece.type, piece.color)
         moves = self.__predict_filter.filter(self.__current_state, moves,
                                              self.get_last_move(), piece.type)
-        return moves
+
+        moves = filter(lambda move: self.will_correct_state_after_move(move),
+                       moves)
+
+        self.__all_possible_moves[cell] = list(moves)
+
+    def will_correct_state_after_move(self, move):
+        game_state = GameState(self.__current_state, move)
+        return self.__post_filter.filter(game_state, move) is not None
 
     def get_last_move(self):
-        return self.__moves[-1] if len(self.__moves) > 0 else None
+        return self.__past_moves[-1] if len(self.__past_moves) > 0 else None
 
     def on_pass_capture(self, cell):
         pass
